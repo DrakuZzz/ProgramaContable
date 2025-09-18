796cfa8204276400d0b90fcef6d53e5156110165
DavidRaHe <david0905.rtf@gmail.com>
2025-09-17 21:58:50 -0600

    Delete src directory

--- src/routes/index.js
+++
@@ -1,5 +0,0 @@
-export{default as cuentasRoutes } from "./cuentas.routes.js"
-
-export{default as aperturaRoutes } from "./addapertura.routes.js"
-
-export{default as facturaRoutes } from "./factura.routes.js"
